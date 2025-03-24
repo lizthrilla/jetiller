38312ae61670c6758c2ac8a8a39c7d7122f065b0
Liz Tiller <elizabeth.tiller@gmail.com>
2025-03-24 18:28:05 -0400

    main info added, needs styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,101 +2,47 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex flex-col items-center justify-items-center min-h-screen gap-10 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <header className="grid grid-rows-1 grid-cols-2 items-center gap-0 md:flex md:flex-wrap">
+        <div className="">
+          <Image src="./butterflyLogos/logo-transparent-svg.svg" alt="Butterfly Financial Consultants Logo" width={500} height={500} className="h-48 w-auto"/>
+        </div>
+        <div className="grid-rows-2 grid-cols-1 font-bf-display ">
+          <h1 className="text-2xl row-start-1 text-bf-blue font-bold"  >
+            Butterfly Financial Consultants
+          </h1>
+          <h2 className="row-start-2">
+            Reinsurance and Acquisition solutions
+          </h2>
+        </div>
+       </header>
+       
+      <main className="flex flex-col items-center justify-center w-screen">
+        <div id="about" className="bg-gray-200 flex flex-col p-10">
+          <p>
+            Butterfly Financial Consultants provides consulting services for mergers and acquisitions with respect to blocks of insurance, entire companies, and reinsurance for both buyers and sellers.  John Tiller, and his wife, Denise, literally wrote the book on reinsurance and can help analyze and structure reinsurance arrangements.  Other services include capital planning, strategic planning, business management, enterprise risk management, as well as expert witness and arbitration.  See our Services page for more information.
+          </p>
+        </div>
+        <div id="book" className="p-10">
+          <h3 className="text-bf-blue font-bf-display font-bold text-xl my-5" > Life, Health and Annuity Reinsurance, Fourth Edition</h3>
+          <p>We are very excited to announce the fourth edition of our book, Life, Health, and Annuity Reinsurance.  Four new chapters have been added: </p>
+          <ul className="px-10">
+            <li className="list-disc">Canadian Regulation of Reinsurance</li>
+            <li className="list-disc">Canadian Reinsurance Accounting and Tax Considerations</li>
+            <li className="list-disc">Risk Transfer Considerations</li>
+            <li className="list-disc">Reinsurance of Inforce Policies</li>
+          </ul>
+          <p>The Reinsurance Treaty chapter has been greatly expanded to compare and contrast US and Canadian treaty language.  The Reinsurance in Other than the US and Canada has also been expanded.</p>
+          <p>For more information about the text and the bibliography, please visit the Life, Health, and Annuity Reinsurance page.   For links to the professional and regulatory organization website and for the Glossary of Reinsurance Terms, please visit our Resources page.
+          To purchase it, go to ACTEX Learning.</p>
+        </div>
+        <div id="why-butterfly" className="bg-gray-200 flex flex-col p-10">
+          <h3 className="text-bf-blue font-bold my-5 font-bf-display text-xl">Why "Butterfly"</h3>
+          <p>Ray Bradbury introduced the idea that the flap of a butterfly's wings. often called the "Butterfly Effect," could have a ripple effect on history in his 1952 short story, The Sound of Thunder. In 1961, Edward Norton Lorenz, an early proponent of chaos theory, noted that tiny variations in data could have a major effect on weather models and likened it to the flap of a butterfly's wing in one part of the world causing tornadoes in another part. John Tiller named his consulting firm Butterfly Financial Consultants because he believes that small actions can have major effects in the insurance world. John's experience, leadership, and creativity provide innovative insurance and reinsurance solutions for problems and clients of all sizes.</p>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="row-start-3 flex gap-[24px] font-bf-display flex-wrap items-center justify-center">
+        Butterfly Financial Consultants - John Tiller -2025
       </footer>
     </div>
   );
