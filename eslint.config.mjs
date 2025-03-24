a7b072167cae3b982276da17ab52221ae9d230e3
Liz Tiller <elizabeth.tiller@gmail.com>
2025-03-24 18:50:02 -0400

    clean up

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+    rules: {
+      quotes: ["error", "single", { avoidEscape: true }],
+    },
+  },
 ];
 
 export default eslintConfig;
